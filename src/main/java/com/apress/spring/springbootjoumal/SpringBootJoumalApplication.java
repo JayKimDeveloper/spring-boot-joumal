71411f21441f538175fe92fb9805e6a7463350b6
unknown <kyhyun1999@gmail.com>
2019-05-08 00:07:09 +0900

    first commit

---
+++ src/main/java/com/apress/spring/springbootjoumal/SpringBootJoumalApplication.java
@@ -0,0 +1,13 @@
+package com.apress.spring.springbootjoumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJoumalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJoumalApplication.class, args);
+    }
+
+}
